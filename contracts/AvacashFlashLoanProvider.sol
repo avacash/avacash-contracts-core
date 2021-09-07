e9dfa55c9dcc1f2b2ad4c1004f6c731ae77ec277
Ealyd <ealyd@protonmail.com>
2021-09-07 14:27:56 -0300

    AvacashFinance contracts V1

---
+++ contracts/AvacashFlashLoanProvider.sol
@@ -0,0 +1,111 @@
+// https://avacash.finance
+/*
+█████╗ ██╗   ██╗ █████╗  ██████╗ █████╗ ███████╗██╗  ██╗
+██╔══██╗██║   ██║██╔══██╗██╔════╝██╔══██╗██╔════╝██║  ██║
+███████║██║   ██║███████║██║     ███████║███████╗███████║
+██╔══██║╚██╗ ██╔╝██╔══██║██║     ██╔══██║╚════██║██╔══██║
+██║  ██║ ╚████╔╝ ██║  ██║╚██████╗██║  ██║███████║██║  ██║
+╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+
+███████╗██╗███╗   ██╗ █████╗ ███╗   ██╗ ██████╗███████╗
+██╔════╝██║████╗  ██║██╔══██╗████╗  ██║██╔════╝██╔════╝
+█████╗  ██║██╔██╗ ██║███████║██╔██╗ ██║██║     █████╗
+██╔══╝  ██║██║╚██╗██║██╔══██║██║╚██╗██║██║     ██╔══╝
+██║     ██║██║ ╚████║██║  ██║██║ ╚████║╚██████╗███████╗
+╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝
+
+*/
+
+pragma solidity 0.5.17;
+interface FlashLoanBorrower {
+  function avacashFlashLoanCall(bytes calldata _data) external payable returns (bool);
+  }
+
+import './libraries/SafeMathUni.sol';
+
+contract AvacashFlashLoanProvider {
+  using SafeMathUni for uint;
+
+  uint private unlocked = 1;
+  address public flashLoanFeeReceiver;
+  uint public flashLoanFee = 3;
+  event FlashLoan(address _recipient, uint256 _amount, bytes _data);
+
+  modifier lock() {
+      require(unlocked == 1, 'AvacashFlashLoanProvider: LOCKED');
+      unlocked = 0;
+      _;
+      unlocked = 1;
+  }
+
+  constructor(address _flashLoanFeeReceiver)  public {
+    flashLoanFeeReceiver = _flashLoanFeeReceiver;
+  }
+
+  function changeFeeReceiver(address _newFeeReceiver) external returns (bool){
+    require(msg.sender == flashLoanFeeReceiver, "Only current flashLoanFeeReceiver can change this value.");
+    require(_newFeeReceiver!= address(0), "New fee receiver should not be address 0");
+    flashLoanFeeReceiver = _newFeeReceiver;
+    return true;
+  }
+
+  function changeFlashLoanFee(uint _newFlashLoanFee) external returns (bool){
+    require(msg.sender == flashLoanFeeReceiver, "Only current flashLoanFeeReceiver can change this value.");
+    flashLoanFee = _newFlashLoanFee;
+    return true;
+  }
+
+  function isContract(address _addr) private view returns (bool){
+  uint32 size;
+  assembly {
+    size := extcodesize(_addr)
+  }
+  return (size > 0);
+}
+
+
+
+  function flashLoan( address _recipient,
+                      uint256 _amount,
+                      bytes calldata _data) external lock returns (bool){
+
+    // 0. Check correct call.
+    require(_amount > 0, "flashLoan(): Please select an positive flashloan _amount.");
+    require(isContract(_recipient), "flashLoan(): _recipient should be a contract.");
+
+    // 1. We check our own balance.
+    uint256  _initialBalance = address(this).balance;
+    require(_initialBalance >= _amount, "flashLoan(): Not enough funds for the flashloan.");
+
+    FlashLoanBorrower _borrower = FlashLoanBorrower(_recipient);
+
+    // 2. We lend the money:
+    (bool success1 ) = _borrower.avacashFlashLoanCall.value(_amount)(_data);
+    // (bool success1, ) = _recipient.call.value(_amount)(abi.encodeWithSignature("avacashFlashLoanCall(bytes32)", _data));
+    require(success1, "flashLoan(): flashloan to _recipient did not go thru.");
+
+    // 3. Calculate _feeAdjusted
+    // fees are in deci-bps, i.e. 1/10th bps https://www.investopedia.com/terms/b/basispoint.asp
+    // if flashLoanFee = 3, means 0.003%
+    uint256 _feeAdjusted = _amount.mul(flashLoanFee);
+
+    require(address(this).balance.mul(100000) >= _initialBalance.mul(100000).add(_feeAdjusted) , "flashLoan(): Not enough fee payed");
+
+    // 4. Send fee to feeReceiver.
+    if (_feeAdjusted > 0 ) {
+      (bool success2, ) = flashLoanFeeReceiver.call.value(address(this).balance.sub(_initialBalance))("");
+      require(success2, "flashLoan(): payment to feeReceiver  did not go thru");
+    }
+
+    //3. We check the final balance.
+    require(address(this).balance == _initialBalance, "flashLoan(): Final balance should be equal to the initial balance.");
+
+    emit FlashLoan(_recipient, _amount,_data);
+    return true;
+  }
+
+  function payBack() external payable returns (bool){
+    return true;
+  }
+
+}
---
+++ contracts/Borrower.sol
@@ -0,0 +1,100 @@
+// https://avacash.finance
+/*
+█████╗ ██╗   ██╗ █████╗  ██████╗ █████╗ ███████╗██╗  ██╗
+██╔══██╗██║   ██║██╔══██╗██╔════╝██╔══██╗██╔════╝██║  ██║
+███████║██║   ██║███████║██║     ███████║███████╗███████║
+██╔══██║╚██╗ ██╔╝██╔══██║██║     ██╔══██║╚════██║██╔══██║
+██║  ██║ ╚████╔╝ ██║  ██║╚██████╗██║  ██║███████║██║  ██║
+╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+
+███████╗██╗███╗   ██╗ █████╗ ███╗   ██╗ ██████╗███████╗
+██╔════╝██║████╗  ██║██╔══██╗████╗  ██║██╔════╝██╔════╝
+█████╗  ██║██╔██╗ ██║███████║██╔██╗ ██║██║     █████╗
+██╔══╝  ██║██║╚██╗██║██╔══██║██║╚██╗██║██║     ██╔══╝
+██║     ██║██║ ╚████║██║  ██║██║ ╚████║╚██████╗███████╗
+╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝
+
+*/
+
+pragma solidity 0.5.17;
+
+import './libraries/SafeMathUni.sol';
+
+
+interface FlashLoanProvider {
+  function flashLoanFee() external returns (uint);
+  function payBack() external payable returns (bool);
+  function flashLoan(address _recipient,uint256 _amount,bytes calldata _data) external returns (bool);
+ }
+
+contract Borrower {
+  using SafeMathUni for uint;
+
+  constructor() public {}
+  event FeeCalculated(uint);
+  uint public a = 0;
+
+  function payableFunction() external payable {}
+
+  function avacashFlashLoanCall(bytes calldata _data) external payable returns (bool) {
+    FlashLoanProvider _flashLoanProvider = FlashLoanProvider(msg.sender);
+    uint _amount = msg.value;
+    uint _flashLoanFee = _flashLoanProvider.flashLoanFee();
+    //emit FeeCalculated(_flashLoanFee);
+    uint _feeAdjusted = _flashLoanFee.mul(_amount);
+    //emit FeeCalculated(_feeAdjusted);
+    uint _fee = _feeAdjusted.div(10000);
+    //emit FeeCalculated(_fee);
+
+    // thief
+    /* emit Data(_data); */
+    bytes memory thief = bytes("thief");
+    bytes memory noFees = bytes("noFees");
+    bytes memory reentrant = bytes("reentrant");
+    bytes memory gasSpender = bytes("gasSpender");
+    /* emit Data( thief); */
+    if (keccak256(thief) == keccak256(_data)) {
+      return true;
+    }
+    else if (keccak256(noFees) == keccak256(_data)) {
+      (bool success) = _flashLoanProvider.payBack.value(msg.value)();
+      require (success, "Error giving back money");
+      return true;
+    }
+    else if (keccak256(reentrant) == keccak256(_data)) {
+      // A reentrant wants to enter again to the flashLoanProvider contract, means:
+      address _provider = msg.sender;
+      address _recipient = address(this);
+      uint _NewAmount = msg.sender.balance;
+      bytes memory _NewData = bytes("");
+      (bool success) = this.flashLoan(_provider, _recipient, _NewAmount,_NewData);
+      require (success, "Error asking for flashloan");
+      return true;
+    }
+    else if (keccak256(gasSpender) == keccak256(_data)) {
+      uint loopsize = 1000;
+      uint i = 0;
+        for (i=0; i<loopsize; i=i+1){
+            a +=1;
+        }
+    }
+    //We just send back the money + fee
+    require (address(this).balance >= msg.value + _fee, "Not enough money to give back");
+    (bool success) = _flashLoanProvider.payBack.value(msg.value + _fee)();
+    require (success, "Error giving back money");
+    return true;
+  }
+
+  event Data(bytes indexed _data);
+  event Print(string indexed w);
+
+  function flashLoan(address _provider, address _recipient, uint _amount, bytes calldata _data) external returns (bool){
+    FlashLoanProvider _flashLoanProvider = FlashLoanProvider(_provider);
+    (bool success) = _flashLoanProvider.flashLoan(_recipient, _amount, _data);
+    require (success, "Error asking for flashloan");
+    emit Data(_data);
+    return true;
+  }
+
+
+}
